9e5c726755d732801be797135a1586c8eaa9df12
Fredhack1 <alfred@altic.digital>
2024-08-04 19:17:16 +0100

    premier commit

---
+++ src/script.ts
@@ -0,0 +1,9 @@
+// export const TALWIND_COLOR = "bg-yellow-500";
+
+const button = document.createElement("button");
+
+button.textContent = "Click me!";
+
+button.classList.add("bg-yellow-500", "text-black");
+
+document.body.appendChild(button);
